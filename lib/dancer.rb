ad2ba4174993aac74ab6f3432d79a9ded10108cd
Yuyi <yuyi365@gmail.com>
2021-11-24 13:41:20 -0500

    commit tree fd327231a73e71b3b3a82ad4fb0557791849038a

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer
+  extend FancyDance::ClassMethods #class
+  include FancyDance::InstanceMethods #instance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  ## include Dance #instance methods
+  ## extend MetaDancing #class methods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
